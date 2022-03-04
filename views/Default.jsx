13a2395f19b1230cf6f5096ec46c86d55428e981
jchung501 <johnchung2011@gmail.com>
2022-03-04 00:17:43 -0600

    added Bootstrap CDN within Default.jsx

--- views/Default.jsx
+++ views/Default.jsx
@@ -10,7 +10,8 @@ class DefaultLayout extends React.Component {
                     <meta httpEquiv='X-UA-Compatible' content='IE edge'/>
                     <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
                     <title>ExSalelent CRM</title>
-                    {/* Milligram CSS for Default Styling */}
+                    {/* BOOTSTRAP CDN*/}
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"></link>
                     {/* Google Fonts */}
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"/>
                     {/* CSS Reset */}
@@ -23,9 +24,9 @@ class DefaultLayout extends React.Component {
                     <link rel="stylesheet" href="/styles.css"/>
                     <script src="/app.js" defer></script>
                 </head>
-                <body>
+                <body className='container-fluid'>
                     <header>
-                        <h1>ExSalelent CRM</h1>
+                        <h1 className='text-center'>ExSalelent CRM</h1>
                         <main>
                             {this.props.children}
                         </main>
