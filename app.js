1b339156accc08902e2184961f7ddcd1756c5ee5
jchung501 <johnchung2011@gmail.com>
2022-03-04 00:11:50 -0600

    update default.jsx and index.jsx to render user first name and their interests, update app.js to include routes to render index.jsx, a random array to seed starter data from

---
+++ app.js
@@ -0,0 +1,113 @@
+/////////////////////////////////////////////
+// Dependencies
+/////////////////////////////////////////////
+
+require('dotenv').config(); // Load ENV variables
+const express = require('express'); // Import Express
+const morgan = require('morgan'); // Import Morgan
+const methodOverride = require('method-override'); // Import Method Override
+const mongoose = require('mongoose'); // Import Mongoose
+const path = require('path');
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+
+const DATABASE_URL = process.env.DATABASE_URL;
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Events Open/Disconnect/Errors
+mongoose.connection
+    .on('open', () => console.log('You\'re connected!'))
+    .on('close', () => console.log('You are disconnected!'))
+    .on('error', (error) => console.log(error));
+
+/////////////////////////////////////////////
+// Models
+/////////////////////////////////////////////
+
+const { Schema, model } = mongoose; // Object Destructuring
+
+// User Schema
+const userSchema = new Schema({
+    first_name: String,
+    last_name: String,
+    phone_number: String,
+    city: String,
+    image: String,
+    interests: [String],
+},
+{
+    timestamps: true,
+});
+
+// Create User Model
+const User = model('User', userSchema);
+
+/////////////////////////////////////////////
+// App Object Setup
+/////////////////////////////////////////////
+
+const app = express();
+app.engine('jsx', require('express-react-views').createEngine());
+app.set('view engine', 'jsx');
+
+/////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////
+
+app.use(morgan('tiny')); // For debugging purposes
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+
+/////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////
+
+app.get('/', (req, res) => {
+    res.send('Welcome to ExSalelent!!');
+});
+
+// Seed 
+app.get('/users/seed', (req, res) => {
+    const starterUsers = [
+        { first_name: 'John', last_name: 'Smith', phone_number: '1234567890', city: 'Kansas City', image: '', interests: ['Mazda', 'Toyota', 'Acura'] },
+        { first_name: 'Zach', last_name: 'Smith', phone_number: '1234567890', city: 'Cool City', image: '', interests: ['Mercedes', 'Audi', 'Lexus'] },
+        { first_name: 'Reginald', last_name: 'Smith', phone_number: '1234567890', city: 'London', image: '', interests: ['Lamborghini', 'Ferrari', 'McLaren'] }
+    ];
+
+    // Delete all fruits
+    User.deleteMany({}).then((data) => {
+        // Seed starter users
+        User.create(starterUsers).then((data) => {
+            res.json(data)
+        });
+    })
+    .catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+// Index
+app.get('/database', (req, res) => {
+    User.find({}).then((users) => {
+        res.render('Index', { users })
+    })
+    .catch((error) => {
+        res.status(400).json({ error });
+    });
+});
+
+/////////////////////////////////////////////
+// Server Listener
+/////////////////////////////////////////////
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Listening to Port ${PORT}`));
---
+++ views/Index.jsx
@@ -0,0 +1,25 @@
+const React = require('react');
+const DefaultLayout = require('./Default.jsx')
+
+class Index extends React.Component {
+    render() {
+        const { users } = this.props;
+        return (
+            <DefaultLayout>
+            <div>
+                {
+                users.map((user) => (
+                    <article>
+                        <a href={`/database/${user._id }`}>
+                            <h2>{user.first_name} - Interested in - {user.interests.join(', ')}</h2>
+                        </a>
+                    </article>
+                ))
+                }
+            </div>
+            </DefaultLayout>
+        );
+    };
+};
+
+module.exports = Index;
