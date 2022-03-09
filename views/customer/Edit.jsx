33eb8c57c21c3067b91140c87bfdf8c1fde25e19
jchung501 <johnchung2011@gmail.com>
2022-03-08 20:16:21 -0600

    update Edit.jsx to show form fields for updating information. added logo img to images folder

--- views/customer/Edit.jsx
+++ views/customer/Edit.jsx
@@ -3,13 +3,24 @@ const DefaultLayout = require('../Default.jsx')
 
 class Edit extends React.Component {
     render() {
-        const { users } = this.props;
+        const { customer } = this.props;
         return (
             <DefaultLayout>
                 <html>
-                    <body>
-                        <h1>Edit Page</h1>
-                    </body>
+                    <form action={`/${customer._id}?_method=PUT`} method="POST">
+                        <fieldset>
+                            <legend>Edit {customer.first_name} {customer.last_name}</legend>
+                            <label>First Name: <input type='text' name='first_name' placeholder='First Name' defaultValue={customer.first_name}/></label>
+                            <label>Last Name: <input type='text' name='last_name' placeholder='Last Name' defaultValue={customer.last_name}/></label>
+                            <label>Phone Number: <input type='text' name='phone_number' placeholder='Phone Number' defaultValue={customer.phone_number}/></label>
+                            <label>Email: <input type='text' name='email' placeholder='Email address' defaultValue={customer.email}/></label>
+                            <label>City: <input type='text' name='city' placeholder='City of Residence' defaultValue={customer.city}/></label>
+                            <label>State: <input type='text' name='state' placeholder='State of Residence' defaultValue={customer.state}/></label>
+                            <label>Image: <input type='url' name='image' placeholder='Image URL' defaultValue={customer.image}/></label>
+                            <label>Interested In: <input type='text' name='interests' placeholder='Please separate interest with commas' defaultValue={customer.interests}/></label>
+                        </fieldset>
+                        <input type='submit' value='Submit Update'/>
+                    </form>
                 </html>
             </DefaultLayout>
         )
